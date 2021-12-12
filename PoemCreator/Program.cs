3978a2fb3a08bc55e11eb8f212ad8f4308e50af9
Mohammad Hossein Karami <mhkarami97@gmail.com>
2021-12-12 08:30:44 +0330

    finish code

--- PoemCreator/Program.cs
+++ PoemCreator/Program.cs
@@ -1,3 +1,58 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Start");
+
+try
+{
+    using var sr = new StreamReader("poems.txt");
+    var poems = new List<string>();
+    var counter = 489;
+    const string resultFolder = "result";
+    var today = DateTime.Now.ToString("yyyy-M-d");
+    string? line;
+    const string layout = "post";
+    const string user = "حافظ";
+    const string tags = "حافظ غزل";
+    const string categories = "poem";
+    const string template = "---" +
+                            "\n" +
+                            $"layout: {layout}" +
+                            "\n" +
+                            $"title: {user}" +
+                            "\n" +
+                            $"tags: {tags}" +
+                            "\n" +
+                            $"categories: {categories}" +
+                            "\n" +
+                            "---" +
+                            "\n" +
+                            "\n";
+
+    while ((line = sr.ReadLine()) != null)
+    {
+        poems.Add(line);
+    }
+
+    Directory.CreateDirectory(resultFolder);
+
+    foreach (var poem in poems)
+    {
+        try
+        {
+            using var sw = new StreamWriter($"{resultFolder}\\{today}-p{counter}.md");
+            sw.WriteLine(template + poem);
+
+            counter++;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+
+    Console.WriteLine("finish");
+}
+catch (Exception e)
+{
+    Console.WriteLine(e);
+}
