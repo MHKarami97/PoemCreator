80d228e625bbd1dcb57ad1c799d4f6afe9eb2a21
mhkarami97 <mhkarami97@gmail.com>
2022-06-06 18:28:08 +0430

    fix bug

--- SiteDataCreator/Image/ImageCreator.cs
+++ SiteDataCreator/Image/ImageCreator.cs
@@ -6,23 +6,23 @@ public static class ImageCreator
     {
         try
         {
-            const int last = 406;
+            const int last = 283;
             const string resultFolder = "resultImage";
 
             const string template = "<p align=\"center\"><img src=\"/assets/img/47-kurdistan/";
             const string template2 = ".jpg\" alt=\"mhkarami97\" /></p>\n";
 
-            const string template3 = "![mhkarami97](/assets/img/47-kurdistan/";
+            const string template3 = "![mhkarami97](/assets/img/55-neor_lake_to_subatan/";
             const string template4 = ".jpg)";
 
             Directory.CreateDirectory(resultFolder);
-            using var sw = new StreamWriter($"{resultFolder}.md");
+            using var sw = new StreamWriter($"{resultFolder}/{resultFolder}.md");
 
             for (var i = 1; i < last; i++)
             {
                 try
                 {
-                    sw.WriteLine(template3 + i.ToString("000") + template4);
+                    sw.WriteLine(template3 + i.ToString("000") + template4+"  \n");
                 }
                 catch (Exception e)
                 {
